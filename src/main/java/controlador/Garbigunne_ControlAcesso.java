0113df3268530ad7f4698df72cb850abf66e3fa0
KoldoAranburu <149675361+KoldoAranburu@users.noreply.github.com>
2024-04-17 10:24:50 +0200

    controlador

---
+++ src/main/java/controlador/Garbigunne_ControlAcesso.java
@@ -0,0 +1,47 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Garbigunne_ControlAcesso
+ */
+@WebServlet("/acces")
+public class Garbigunne_ControlAcesso extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Garbigunne_ControlAcesso() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String correo = (String) request.getParameter("correo");
+		System.out.println(correo);
+		if (correo.equalsIgnoreCase("admin@ee")) {
+			request.getRequestDispatcher("/Paneles_control/Admin/");
+		} else {
+			response.sendRedirect("http://localhost:8080/Garbigune_reto/productos");
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
