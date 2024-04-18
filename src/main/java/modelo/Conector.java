300b09f45e82bbeb91e10c1b578ddeca9bf0e658
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2024-04-18 09:50:00 +0200

    Añadimos las tablas

--- src/main/java/modelo/Conector.java
+++ src/main/java/modelo/Conector.java
@@ -2,18 +2,45 @@ package modelo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
 public class Conector {
 	protected Connection conexion;
 
     Conector(){
+//        try {
+//            Class.forName("oracle.jdbc.driver.OracleDriver");
+//            String url = "jdbc:oracle:thin:@" + Config.HOST + ":" + Config.PORT + ":" + Config.SID;
+//            this.conexion = DriverManager.getConnection(url, Config.USERNAME, Config.PASSWORD);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        
+      	//URL of Oracle database server
+        String url = "jdbc:oracle:thin:@" + Config.HOST + ":" + Config.PORT + ":xe";       
+        Properties props = new Properties();
+        props.setProperty("user", Config.USERNAME);
+        props.setProperty("password", Config.USERNAME);
+      
+        //creating connection to Oracle database 
+      
         try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            String url = "jdbc:oracle:thin:@" + Config.HOST + ":" + Config.PORT + ":" + Config.SID;
-            this.conexion = DriverManager.getConnection(url, Config.USERNAME, Config.PASSWORD);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+			this.conexion = DriverManager.getConnection(url,props);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			Statement st = conexion.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        System.out.println("Se ha conectado a la BBDD-Oracle satisfactoriamente");
+
     }
 
     public Connection getConexion() {
