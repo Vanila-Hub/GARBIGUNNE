49a55a4893d58a6ec47fb7c2751758b11471f3fb
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2024-04-19 08:08:45 +0200

    ya va

--- src/main/java/controlador/Borrar.java
+++ src/main/java/controlador/Borrar.java
@@ -10,16 +10,16 @@ import javax.servlet.http.HttpServletResponse;
 import modelo.ModeloPlanta;
 
 /**
- * Servlet implementation class Create
+ * Servlet implementation class Borrar
  */
-@WebServlet("/create")
-public class Create extends HttpServlet {
+@WebServlet("/borrar")
+public class Borrar extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Create() {
+    public Borrar() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,32 +29,29 @@ public class Create extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		String opcion = (String) request.getParameter("opcion");
+		switch (opcion) {
+		case "planta":	
+			
+			int id_planta = Integer.parseInt(request.getParameter("id"));
+			
+//lalam al modelo para inser
+			ModeloPlanta modelo_planta = new ModeloPlanta();
+			modelo_planta.borrarPlantaByID(id_planta);
+			//volvera el /plantas
+			response.sendRedirect("http://localhost:8080/Garbigune_reto/admin?peticion=planta");
+			break;
+			
+		default:
+			break;
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String opcion = (String) request.getParameter("opcion");
-		switch (opcion) {
-		case "planta":
-			String nombre = (String) request.getParameter("nombre");
-			String dirrecion = (String) request.getParameter("direccion");
-			String telefono = (String) request.getParameter("telefono");
-			System.out.println(nombre+dirrecion+telefono);
-//lalam al modelo para inser
-			ModeloPlanta modelo_planta = new ModeloPlanta();
-			modelo_planta.crearPlanta(nombre,dirrecion,telefono);
-			//volvera el /plantas
-			response.sendRedirect("http://localhost:8080/Garbigune_reto/admin?peticion=planta");
-			break;
-
-		default:
-			break;
-		}
 		
-
 	}
 
 }
