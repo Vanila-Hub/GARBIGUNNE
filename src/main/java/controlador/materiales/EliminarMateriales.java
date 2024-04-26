1429fef0450d84f176bb185f8f7a2dc81aedcb0d
KoldoAranburu <149675361+KoldoAranburu@users.noreply.github.com>
2024-04-26 08:21:01 +0200

    materiales

---
+++ src/main/java/controlador/materiales/EliminarMateriales.java
@@ -0,0 +1,47 @@
+package controlador.materiales;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.ModeloMaterial;
+
+/**
+ * Servlet implementation class EliminarMateriales
+ */
+@WebServlet("/EliminarMateriales")
+public class EliminarMateriales extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EliminarMateriales() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id_material = Integer.parseInt(request.getParameter("id"));
+		
+		ModeloMaterial modelo_material = new ModeloMaterial();
+		modelo_material.borrarMaterialByID (id_material);
+		
+		response.sendRedirect("http://localhost:8080/Garbigune_reto/VerMateriales/materiales");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
