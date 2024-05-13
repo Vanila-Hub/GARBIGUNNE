181b843e9d65b9be411648aa001f45e910dc981b
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2024-05-13 12:11:24 +0200

    Testes

--- src/main/java/modelo/emisionproducto/EmisionProducto.java
+++ src/main/java/modelo/emisionproducto/EmisionProducto.java
@@ -5,51 +5,55 @@ import java.sql.Date;
 import modelo.productos.Producto;
 
 public class EmisionProducto {
-    private int id_emision;
-    private int id_producto;
-    private double emision_generada;
-    private Date fecha;
-    
-    private Producto producto;
+	private int id_emision;
+	private int id_producto;
+	private double emision_generada;
+	private Date fecha;
 
-    public EmisionProducto() {
-    }
+	private Producto producto;
 
-    public EmisionProducto(int id_producto, int id_material, double emision_generada, Date fecha) {
-    	super();
-    }
+	public EmisionProducto() {
+	}
 
-    public int getId_emision() {
-        return id_emision;
-    }
+	public EmisionProducto(int id_producto, int id_material, double emision_generada, Date fecha) {
+		this.producto = producto;
+		this.id_producto = id_producto;
+		this.fecha = fecha;
+		this.emision_generada = emision_generada;
+		this.id_emision = id_emision;
+	}
 
-    public void setId_emision(int id_emision) {
-        this.id_emision = id_emision;
-    }
+	public int getId_emision() {
+		return id_emision;
+	}
 
-    public int getId_producto() {
-        return id_producto;
-    }
+	public void setId_emision(int id_emision) {
+		this.id_emision = id_emision;
+	}
 
-    public void setId_producto(int id_producto) {
-        this.id_producto = id_producto;
-    }
+	public int getId_producto() {
+		return id_producto;
+	}
 
-    public double getEmision_generada() {
-        return emision_generada;
-    }
+	public void setId_producto(int id_producto) {
+		this.id_producto = id_producto;
+	}
 
-    public void setEmision_generada(double emision_generada) {
-        this.emision_generada = emision_generada;
-    }
+	public double getEmision_generada() {
+		return emision_generada;
+	}
 
-    public Date getFecha() {
-        return fecha;
-    }
+	public void setEmision_generada(double emision_generada) {
+		this.emision_generada = emision_generada;
+	}
 
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
 
 	public Producto getProducto() {
 		return producto;
@@ -64,6 +68,5 @@ public class EmisionProducto {
 		return "EmisionProducto [id_emision=" + id_emision + ", id_producto=" + id_producto + ", emision_generada="
 				+ emision_generada + ", fecha=" + fecha + ", producto=" + producto + "]";
 	}
-
 
 }
