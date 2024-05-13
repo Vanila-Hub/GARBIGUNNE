4617ea2718aae69b7e4fcff6537473be72b593be
Vanila-Dev <149675635+Vanila-Hub@users.noreply.github.com>
2024-05-13 09:13:09 +0200

    cambios eñaut

---
+++ src/main/java/modelo/test/ModeloPlantaTest.java
@@ -0,0 +1,34 @@
+package modelo.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ModeloPlantaTest {
+
+	@Test
+	void testGetPlantas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCrearPlanta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBorrarPlantaByID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPlantaByID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testActualizar() {
+		fail("Not yet implemented");
+	}
+
+}
